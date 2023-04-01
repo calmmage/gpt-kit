7456f1d71254fcfb90604bfbcce183b9860e24b1
Petr Lavrov <calmquant@gmail.com>
2023-04-01 22:56:38 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@
 
 from pathlib import Path
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='gpt-kit',
     description='OOP wrapper and utils for OpenAI GPT API',
-    version='0.0.1',
-    packages=['gpt_kit'],
+    version='0.0.2',
+    packages=find_packages(),
     author='Petr Lavrov',
     author_email='petr.b.lavrov@gmail.com',
     long_description=Path('README.md').read_text(),
     install_requires=Path('requirements.txt').read_text().split(),
-    url='https://github.com/calmmage/gpt_api',
+    url='https://github.com/calmmage/gpt_kit',
     py_modules=["gpt_kit"],
     long_description_content_type="text/markdown",
     classifiers=[
