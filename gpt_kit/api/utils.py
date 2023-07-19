d96a282abc49e5b85c027b1f7b02b38ef4f80d17
Petr Lavrov <calmquant@gmail.com>
2023-07-19 04:10:49 +0300

    use tiktoken instead of transformer
    - for speed
    - to fix the test, as transformers are optional dependency
    - update pyproject.toml

--- gpt_kit/api/utils.py
+++ gpt_kit/api/utils.py
@@ -16,61 +16,17 @@ def init_gpt_api_key():
 # Token counts
 # ----------------------------
 
-ru_pattern = re.compile(r'[а-яА-ЯёЁ]')
-en_pattern = re.compile(r'[a-zA-Z ]')
-digit_pattern = re.compile(r'[0-9]')
-other_pattern = re.compile(r'[^а-яА-ЯёЁa-zA-Z0-9 ]')
 
-
-def get_token_count_estimate(text):
-    """Estimate the number of tokens in a text.
-    for precise count: from gpt_api import get_token_count
-    or
-    import transformers
-    tokenizer = transformers.GPT2TokenizerFast.from_pretrained("gpt2")
-    return len(tokenizer.encode(text))
+def get_token_count(text, model="gpt-3.5-turbo"):
     """
-    # Precompile the regex patterns
-
-    # Count russian characters
-    ru_count = len(ru_pattern.findall(text))
-    # print("ru", ru_count)
-
-    # Count english characters
-    en_count = len(en_pattern.findall(text))
-    # print("en", en_count)
-
-    # Count digits
-    digit_count = len(digit_pattern.findall(text))
-    # print("digit", digit_count)
-
-    # Count other characters
-    other_count = len(other_pattern.findall(text))
-    # print("other", other_count)
-
-    # Calculate the total count
-    res = math.ceil(1.2 * ru_count) + \
-          (en_count + 3) // 4 + \
-          (1.5 * digit_count + 1) // 2 + \
-          math.ceil(1.5 * other_count)
-    return int(res)
-
-
-estimate_tokens = get_token_count_estimate
-
-
-def _get_token_count(text):
-    # calculate amount of tokens in text
-    import transformers
-    tokenizer = transformers.GPT2TokenizerFast.from_pretrained("gpt2")
-    return len(tokenizer.encode(text))
-
-
-def get_token_count(text, exact=False):
-    if exact:
-        return _get_token_count(text)
-    else:
-        return get_token_count_estimate(text)
+    calculate amount of tokens in text
+    model: gpt-3.5-turbo, gpt-4, text-davinci-003, text-curie-001, code-davinci-edit-001, text-davinci-edit-001, code-davinci-002
+    """
+    import tiktoken
+    model = parse_model(model)
+    # To get the tokeniser corresponding to a specific model in the OpenAI API:
+    enc = tiktoken.encoding_for_model(model)
+    return len(enc.encode(text))
 
 
 # ----------------------------
--- tests/gpt_kit/api/test_utils.py
+++ tests/gpt_kit/api/test_utils.py
@@ -1,28 +1,28 @@
-from gpt_kit.api.utils import get_token_count_estimate, _get_token_count
 import pytest
 
+from gpt_kit.api.utils import get_token_count
+
 queries = [
-    ("This is a test. Write a fun poem about spinning tops:", 16, 13),
-    ("Русский текст. Write a fun poem about spinning tops:", 28, 24),
+    ("This is a test. Write a fun poem about spinning tops:", 13),
+    ("Русский текст. Write a fun poem about spinning tops:", 14),
     ("User1, user2, 6749589, pep, Русский текст. "
-     "Write a fun poem about spinning tops:", 45, 37),
-    ("Emojis: 🤔, ✅, ??, ??", 18, 15),
-    ("Давайте посмотрим, что будет, если мы введем русский текст", 63, 62),
-    ("",  0, 0),
-    ("12",  2, 1),
-    ("a",  1, 1),
-    ("user",  1, 1),
-    ("1", 1, 1),
-    ("ц", 2, 2),
-    ("я", 2, 1),
+     "Write a fun poem about spinning tops:", 27),
+    ("Emojis: 🤔, ✅, ??, ??", 13),
+    ("Давайте посмотрим, что будет, если мы введем русский текст", 22),
+    ("", 0),
+    ("12", 1),
+    ("a", 1),
+    ("user", 1),
+    ("1", 1),
+    ("ц", 1),
+    ("я", 1),
 ]
-@pytest.mark.parametrize("text,est,real", queries)
-def test_get_token_count(text,est,real):
-    estimate = get_token_count_estimate(text)
-    token_count = _get_token_count(text)
+
+
+@pytest.mark.parametrize("text,real", queries)
+def test_get_token_count(text, real):
+    token_count = get_token_count(text)
     assert token_count == real
-    assert estimate == est
-    assert estimate >= token_count
 
 # todo: test parse_model
 
